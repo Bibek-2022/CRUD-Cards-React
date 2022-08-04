557a733276ebb452cab326a2753c03075ec8533c
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-04 22:14:58 +1000

    Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
-export const Login = () => {
-  return <div>Login</div>;
+import { useDispatch } from "react-redux";
+
+const LoginForm = () => {
+  const [form, setForm] = useState({});
+  const dispatch = useDispatch();
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleOnSubmit = (e) => {};
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Form.Group className="mb-3 " controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          onChange={handleOnChange}
+          required
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="*********"
+          onChange={handleOnChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Log In
+      </Button>
+
+      <div className="text-end">
+        Forgot Password <a href="/password-reset">Reset </a>Now
+      </div>
+    </Form>
+  );
 };
+
+export default LoginForm;
