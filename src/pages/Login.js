c5864e403a569a3e29a86209ea7ab7abcd0ee115
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-11 22:42:54 +1000

    Sign in form MO

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,39 +9,39 @@ const LoginForm = () => {
     setForm({ ...form, [name]: value });
   };
   const handleOnSubmit = (e) => {};
+
   return (
-    <Card>
-      <Form onSubmit={handleOnSubmit}>
-        <Form.Group className="mb-3 " controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            onChange={handleOnChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="*********"
-            onChange={handleOnChange}
-            required
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Log In
-        </Button>
-
-        <div className="text-end">
-          Forgot Password <a href="/password-reset">Reset </a>Now
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
         </div>
-      </Form>
-    </Card>
+      </form>
+    </div>
   );
 };
 
