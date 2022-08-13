2518669b642b290dfcfa8bf97371a597394ebbbd
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-08-13 22:28:56 +1000

    Main Layout

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
 
-export const MainLayout = () => {
-  return <div>MainLayout</div>;
+export const MainLayout = ({ children }) => {
+  return (
+    <div>
+      {/* header */}
+      <Header />
+
+      {/* mainbody */}
+      <main className="main container">{children}</main>
+
+      {/* footer */}
+      <Footer />
+    </div>
+  );
 };
